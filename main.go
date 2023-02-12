746f7b41186852e8c74c9ea20b7549b6797f0123
Thymios <douvisthym@gmail.com>
2023-02-12 13:01:41 +0200

    animal package

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"models/animal"
+	"os"
+)
+
+func main() {
+	// open file
+	file, err := os.Open("data/animals.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// remember to close the file at the end of the program
+	defer file.Close()
+
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(file)
+
+	data, err := csvReader.ReadAll()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	animals := animal.CreateManyfromCSV(data)
+
+	fmt.Println(len(animals))
+
+	// do something with read line
+	fmt.Printf("%v\n", animals)
+}
---
+++ vendor/models/animal/animal.go
@@ -0,0 +1,29 @@
+package animal
+
+type Animal struct {
+	Name     string
+	Logogram string
+	Emoji    string
+}
+
+func CreateManyfromCSV(data [][]string) []Animal {
+	var animals []Animal
+	for i, row := range data {
+		if i < 1 {
+			continue
+		}
+		var animal Animal
+		for j, field := range row {
+			switch j {
+			case 0:
+				animal.Name = field
+			case 1:
+				animal.Logogram = field
+			case 2:
+				animal.Emoji = field
+			}
+		}
+		animals = append(animals, animal)
+	}
+	return animals
+}
